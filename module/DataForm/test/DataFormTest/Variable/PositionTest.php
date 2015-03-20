b16bf9a5b174733fafe7e19f16c75bd13efac438
Ross Basarevych <basarevych@gmail.com>
2015-03-20 21:07:12 +0200

    ShortFullName var test

---
+++ module/DataForm/test/DataFormTest/Variable/PositionTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace DataFormTest\Variable;
+
+use Zend\Json\Json;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use Application\Entity\Template as TemplateEntity;
+use Application\Entity\Client as ClientEntity;
+use DataForm\Document\Profile as ProfileDocument;
+use DataForm\Variable\Position as PositionVariable;
+
+class PositionTest extends AbstractHttpControllerTestCase
+{
+    public function setUp()
+    {
+        $this->setApplicationConfig(require 'config/application.config.php');
+
+        parent::setUp();
+
+        $this->doc = new ProfileDocument();
+
+        $this->dm = $this->getMockBuilder('Doctrine\ODM\MongoDB\DocumentManager')
+                         ->disableOriginalConstructor()
+                         ->setMethods([ 'getRepository' ])
+                         ->getMock();
+
+        $this->repo = $this->getMockBuilder('DataForm\Document\ProfileRepository')
+                           ->disableOriginalConstructor()
+                           ->setMethods([ 'find' ])
+                           ->getMock();
+
+        $this->repo->expects($this->any())
+                   ->method('find')
+                   ->will($this->returnValue($this->doc));
+
+        $this->dm->expects($this->any())
+                 ->method('getRepository')
+                 ->will($this->returnValue($this->repo));
+
+        $this->dfm = $this->getMockBuilder('Application\Service\DataFormManager')
+                          ->disableOriginalConstructor()
+                          ->setMethods([ 'getDocumentClass' ])
+                          ->getMock();
+
+        $this->dfm->expects($this->any())
+                  ->method('getDocumentClass')
+                  ->will($this->returnValue('DocumentClass'));
+
+        $this->sl = $this->getApplicationServiceLocator();
+        $this->sl->setAllowOverride(true);
+        $this->sl->setService('doctrine.documentmanager.odm_default', $this->dm);
+        $this->sl->setService('DataFormManager', $this->dfm);
+    }
+
+    public function testExecute()
+    {
+        $var = new PositionVariable();
+        $var->setServiceLocator($this->sl);
+        $var->setTemplate(new TemplateEntity());
+        $var->setClient(new ClientEntity());
+
+        ob_start();
+        $var->execute('foobar');
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertEquals('foobar', $output);
+
+        $this->doc->setPosition('position');
+
+        ob_start();
+        $var->execute();
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertEquals('position', $output);
+    }
+}
